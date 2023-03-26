3a0d67ff8b8cda4fd1b962b71a81d287958e5e21
Tatsuya Miyazaki <miyazaki.tatsuya.mail@gmail.com>
2023-03-26 16:20:00 +0900

    [TelloCharger] Change to use ServoESP32 of ArduinoCommon

--- src/TelloCharger/src/main.cpp
+++ src/TelloCharger/src/main.cpp
@@ -1,31 +1,40 @@
-#include "AtomMotion.h"
-#include <M5Atom.h> //Atomのヘッダファイルを準備
+#include <Arduino.h>
+#include <M5Atom.h>
+#include <ServoESP32.h>
+#include <Timer.h>
 
-AtomMotion Atom;
-
-static const uint8_t DRONE_RELEASE_ANGLE = 18;
-static const uint8_t DRONE_CATCH_ANGLE = 94;
-static const uint8_t USB_OFF_ANGLE = 41;
-static const uint8_t USB_ON_ANGLE = 130;
-static uint8_t mainServoAngle = 90;
-static uint8_t subServoAngle = 90;
+static const int16_t DRONE_RELEASE_ANGLE = -31;
+static const int16_t DRONE_CATCH_ANGLE = 8;
+static const int16_t USB_OFF_ANGLE = -56;
+static const int16_t USB_ON_ANGLE = 50;
+static const float SERVO1_ANG_VEL = 45;
+static const float SERVO2_ANG_VEL = 150;
+static int16_t mainServoAngle = DRONE_RELEASE_ANGLE;
+static int16_t subServoAngle = USB_OFF_ANGLE;
 static uint8_t chargeStep = 0;
 
+// サーボの指示角度[deg]（-90deg～90deg）
+float angle = 0.0f;
+// サーボ指示角度の増減値
+float delta = 90.0f;
+// サーボの回転角速度[deg/s]
+float speed = 90.0f;
+
+// 1つめのRCサーボをledcチャンネル0でIO26に設定
+ServoESP32 servo1 = ServoESP32(0, 19, DRONE_RELEASE_ANGLE, SERVO1_ANG_VEL);
+// 2つめのRCサーボをledcチャンネル1でIO27に設定
+ServoESP32 servo2 = ServoESP32(2, 23, USB_OFF_ANGLE, SERVO2_ANG_VEL);
+// 繰り返し動作用のタイマーを作成
+Timer timer = Timer(3000);
+
 void GetStatus() {
-  Serial.printf("Servo Angle (1, 2, 3, 4) = (%d, %d, %d, %d)\n",
-                Atom.ReadServoAngle(1), Atom.ReadServoAngle(2),
-                Atom.ReadServoAngle(3), Atom.ReadServoAngle(4));
+  Serial.printf("Servo Angle (1, 2) = (%f, %f)\n", servo1.getTargetAngle(),
+                servo2.getTargetAngle());
 }
 
 void setup() {
-  M5.begin(true, false, true);
-  Atom.Init(); // sda  25     scl  21
-  delay(1000);
-  subServoAngle = USB_OFF_ANGLE;
-  Atom.SetServoAngle(2, subServoAngle);
-  delay(3000);
-  mainServoAngle = DRONE_RELEASE_ANGLE;
-  Atom.SetServoAngle(1, mainServoAngle);
+  Serial.begin(115200);
+  Serial.println("Start example of ServoESP32");
   delay(3000);
 }
 
@@ -33,50 +42,53 @@ void loop() {
   M5.update();
   if (M5.Btn.wasPressed()) {
     chargeStep = (chargeStep + 1) % 4;
+    Serial.println("BUTTON!!! " + String(chargeStep));
     switch (chargeStep) {
     case 0:
+      // ドローンをリリースする
+      mainServoAngle = DRONE_RELEASE_ANGLE;
+      subServoAngle = USB_OFF_ANGLE;
+      break;
+    case 1:
       // ドローンをキャッチする
       mainServoAngle = DRONE_CATCH_ANGLE;
       subServoAngle = USB_OFF_ANGLE;
       break;
-    case 1:
+    case 2:
       // 充電開始する
       subServoAngle = USB_ON_ANGLE;
       break;
-    case 2:
-      // 充電終了する
-      subServoAngle = USB_OFF_ANGLE;
-      break;
     case 3:
-      // ドローンをリリースする
-      mainServoAngle = DRONE_RELEASE_ANGLE;
+      // 充電終了する
       subServoAngle = USB_OFF_ANGLE;
       break;
     default:
       break;
     }
-    Atom.SetServoAngle(2, subServoAngle);
-    Atom.SetServoAngle(1, mainServoAngle);
+    servo1.setTargetAngle(mainServoAngle);
+    servo2.setTargetAngle(subServoAngle);
     GetStatus();
   }
   if (Serial.available()) { // シリアルデータが利用可能であれば
     char input = Serial.read(); // シリアルデータを読み込む
     if (input == 'w') {         // もし 'w' キーが押されたら
       subServoAngle++;          // 変数Aをインクリメントする
-      Atom.SetServoAngle(2, subServoAngle);
+      servo2.setTargetAngle(subServoAngle);
       GetStatus();
     } else if (input == 's') { // もし 's' キーが押されたら
       subServoAngle--;         // 変数Aをデクリメントする
-      Atom.SetServoAngle(2, subServoAngle);
+      servo2.setTargetAngle(subServoAngle);
       GetStatus();
     } else if (input == 'a') { // もし 'a' キーが押されたら
       mainServoAngle--;        // 変数Bをデクリメントする
-      Atom.SetServoAngle(1, mainServoAngle);
+      servo1.setTargetAngle(mainServoAngle);
       GetStatus();
     } else if (input == 'd') { // もし 'd' キーが押されたら
       mainServoAngle++;        // 変数Bをインクリメントする
-      Atom.SetServoAngle(1, mainServoAngle);
+      servo1.setTargetAngle(mainServoAngle);
       GetStatus();
     }
   }
+  servo1.loop();
+  servo2.loop();
 }
