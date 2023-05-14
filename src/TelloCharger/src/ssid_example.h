34ae0a09ed3d25f5ff654e6048e1c81983a183ef
Tatsuya Miyazaki <miyazaki.tatsuya.mail@gmail.com>
2023-05-14 18:14:13 +0900

    [TelloCharger] Add example of ssid.h

---
+++ src/TelloCharger/src/ssid_example.h
@@ -0,0 +1,11 @@
+/**
+ * @file ssid_example.h
+ * @brief SSID情報を記述するヘッダファイルの例
+ *
+ * @details 環境に合わせて、ファイル名をssid.hにリネームして利用してください（ssid.hはgitignoreされている）
+ */
+
+#pragma once
+
+#define WIFI_SSID "XXXXXXXXXXX"
+#define WIFI_PASSWORD "XXXXXXXX"
